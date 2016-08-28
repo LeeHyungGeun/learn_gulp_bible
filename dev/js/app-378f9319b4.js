26266e601245fce4c2b377592e39ff395da39d0f
LeeHyungGeun <lhg4031@gmail.com>
2016-08-28 12:19:15 +0800

    main develop files

---
+++ dev/js/app-378f9319b4.js
@@ -0,0 +1,1 @@
+console.log("Hello World");var div=document.createElement("div");div.innerHTML="<textarea>Hello World5</textarea>",document.body.appendChild(div);for(var i=0;i<100;i++){var div=document.createElement("div");div.innerHTML="<span>Hello World</span>",document.body.appendChild(div)}
